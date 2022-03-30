f15d30523157a346fd06de76d1b0b095d37814b1
slo-1c-1-2021 <81393642+slo-1c-1-2021@users.noreply.github.com>
2022-03-30 07:46:47 +0200

    Add files via upload

---
+++ 2011_pp_zadanieASCII.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string s;
+    ifstream f("hasla.txt");
+    int suma;
+    while (f>>s);
+{
+
+        for (int i=0; i<a; i<s.size ()-1; i++)
+        {
+
+        }
+    }
+
+}
